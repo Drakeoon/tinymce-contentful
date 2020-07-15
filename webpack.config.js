6bb4b1fbb35a35a86df5b77580a5764686d8de1e
Daniel Szczepanik <tmlm18@gmail.com>
2020-07-15 15:46:45 +0200

    Improve develpoment config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+const path = require("path");
+
 module.exports = {
   watch: true,
   devtool: "source-map",
   mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+  },
 };
