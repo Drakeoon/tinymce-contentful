f5758a11dd36fcf16416ade68307eb50a6d2b5e8
Daniel Szczepanik <tmlm18@gmail.com>
2020-07-16 13:57:53 +0200

    Add ability to create/edit links

--- src/tinymce/defaults.js
+++ src/tinymce/defaults.js
@@ -1,10 +1,10 @@
-const defaultMenubar = "edit view insert format tools extra";
+const defaultMenubar = "edit view insert format tools";
 
 const defaultToolbar =
-  "extra codesample | undo redo | formatselect | bold italic | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent";
+  "extra | undo redo | formatselect | bold italic link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent";
 
 const defaultPlugins = [
-  "autoresize textpattern lists advlist media image code extra",
+  "autoresize textpattern lists advlist media image code extra link",
 ];
 
 export { defaultMenubar, defaultToolbar, defaultPlugins };
